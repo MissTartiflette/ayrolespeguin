cc647bafe3bd77cf39522dd75ee3ec577363debf
yoanayroles <yoan3007@hotmail.fr>
2018-10-09 19:05:11 +0200

    Ajout du fichier Element.cpp

--- src/shared/state/Element.cpp
+++ src/shared/state/Element.cpp
@@ -1,4 +1,15 @@
 #include "state.h"
-#include <iostream>
 
 using namespace state;
+
+Element::Element(){
+	/*this -> nom = "";
+	Position position;
+	position.setX(2);
+	Statistiques statistiques;
+	statistiques.setAttaque(4);*/
+}
+
+std::string Element::getNom(){
+	return nom;
+}
