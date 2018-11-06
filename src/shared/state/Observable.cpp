fa3aa0fdc77bb2128b0f902e3663e90213a73476
chloepeguin <chloe.aurore.p@sfr.fr>
2018-11-06 16:49:42 +0100

    observable.cpp

--- src/shared/state/Observable.cpp
+++ src/shared/state/Observable.cpp
@@ -1,13 +1,17 @@
 #include "state.h"
 #include "engine.h"
+#include <iostream>
+
 using namespace state;
- 
+using namespace std;
+
 void Observable::registerObserver (IObserver* observer){
 	observers.push_back(observer);
 }
 
-void Observable::notifyObservers (const StateEvent& e, Etat& etat){
+void Observable::notifyObservers (const StateEvent& e, Etat& etat, sf::RenderWindow& window){
+	
 	for(auto observer : observers){
-		observer->stateChanged(e, etat);
+		observer->stateChanged(e, etat, window);
 	}
 }
